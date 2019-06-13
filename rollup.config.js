5672b69b90dc01836c4915af633337558d1d3bc4
Leonardo Almeida Marques <leonardo@finqle.nl>
2019-06-13 16:39:11 +0200

    adde rollup and node-sass for builds, adjust eslintrc

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel'
+// import filesize from 'rollup-plugin-filesize';
+// import progress from 'rollup-plugin-progress';
+// import visualizer from 'rollup-plugin-visualizer';
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+process.env.NODE_ENV = 'production'
+process.env.BABEL_ENV = 'production'
+
+const config = {
+  input: 'src/cameraclick.js',
+  output: {
+    file: 'dist/cameraclick.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    json(),
+    // visualizer(),
+    // filesize(),
+    // progress(),
+    terser(),
+    resolve({
+      browser: true,
+      extensions: ['.js']
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({ runtimeHelpers: true })
+  ]
+}
+
+export default config
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import CameraClick from './cameraclick'
 import './cameraclick.scss'
+window.onload = () => {
+  const createElement = (data, width, height) => {
+    const img = document.createElement('img')
+    img.id = 'ai'
+    img.style.width = `${element.clientWidth}px`
+    img.style.height = `${element.clientHeight}px`
+    img.style.float = 'right'
+    img.src = data.image
+    img.style.objectFit = 'cover'
+    const canvas = document.createElement('canvas')
+    canvas.width = element.clientWidth
+    canvas.height = element.clientHeight
+    canvas.getContext('2d').drawImage(img, 0, 0)
 
-const canvas = document.querySelector('#camera')
-const camera = new CameraClick(canvas, {
-  resolution: 'hd',
-  onCapture: function (data) {
-    console.log('here is the data ', data)
+    console.log('here is the data ', img)
   }
-})
 
-console.log(camera)
+  const element = document.querySelector('#camera')
+  const camera = new CameraClick(element, {
+    onCapture: function (data) {
+      createElement(data)
+    }
+  })
+
+  const start = document.querySelector('#start')
+  start.onclick = function () {
+    element.classList.toggle('open')
+    if (element.classList.contains('open')) {
+      camera.open()
+    } else {
+      setTimeout(() => { camera.close() }, 300)
+    }
+  }
+  console.log(camera)
+}
