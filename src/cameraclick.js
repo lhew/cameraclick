53eb11b0104fef8cb0b3c2a1aaeacfb45ce37a78
Leonardo Almeida Marques <leonardo@finqle.nl>
2019-06-13 16:41:42 +0200

    adjusted build parameters and file linting

--- src/cameraclick.js
+++ src/cameraclick.js
@@ -1,1 +1,111 @@
-"use strict";function CameraClick(e,t){let a=!1;const n={width:320,height:480,screenshotCaption:"Screenshot",resolution:"auto",captureCaption:{start:"Start",stop:"Stop"},...t};let o={};const i=e,c=document.createElement("button"),s=document.createElement("video"),r=document.createElement("div"),l=document.createElement("div"),p=(e={})=>{const a={type:"image/jpeg",quality:.85,stopStreamAfterCapture:!0,...e},n=document.createElement("canvas"),{width:i,height:c}=[...o.getVideoTracks()].filter(e=>e.enabled)[0].getSettings();if(n.width=i,n.height=c,n.getContext("2d").drawImage(s,0,0),"function"==typeof t.onCapture){const{type:e,quality:o}=a;t.onCapture({image:n.toDataURL(e,o),cropInfo:{}})}},d=async function(){const e=await navigator.permissions.query({name:"camera"});return(await e).state};return i.className="cameraclick",l.className=`${e.className||"element"}__wrapper`,r.className=`${e.className||"element"}__video-wrapper`,c.className=`${e.className||"element"}__capture`,c.innerHTML=n.screenshotCaption,c.disabled=!0,c.onclick=p,s.autoplay=!0,s.width=n.width,s.height=n.height,s.style.objectFit="cover",l.appendChild(r),l.appendChild(c),i.appendChild(l),{isOpen:()=>a,capture:p,open:async(e,t)=>{"function"==typeof e&&e("granted"===await d()),a=!0,c.disabled=!1,navigator.getUserMedia({video:!0},function(e){r.innerHTML="",r.appendChild(s),s.srcObject=e,o=e,window.mediaTracks=o},function(e){"function"==typeof t&&t(e)})},close:e=>{c.disabled=!0,a=!1,s.srcObject=null,[...o.getTracks()].map(e=>e.stop()),"function"==typeof t.onClose&&e&&t.onClose()}}}module.exports=CameraClick;//# sourceMappingURL=cameraclick.js.map
+export default function CameraClick (element, camOptions) {
+  let isPlaying = false
+
+  const options = {
+    width: 320,
+    height: 480,
+    screenshotCaption: 'Screenshot',
+    resolution: 'auto',
+    captureCaption: { start: 'Start', stop: 'Stop' },
+    ...camOptions
+  }
+
+  let mediaTracks = {}
+  const videoElement = element
+
+  const captureBtn = document.createElement('button')
+  const videoCanvas = document.createElement('video')
+  const videoWrapper = document.createElement('div')
+  const wrapper = document.createElement('div')
+
+  const open = async (onOpen, onError) => {
+    if (typeof onOpen === 'function') {
+      onOpen((await checkPermissions()) === 'granted')
+    }
+
+    isPlaying = true
+    captureBtn.disabled = false
+
+    navigator.getUserMedia(
+      { video: true },
+      function (localMediaStream) {
+        videoWrapper.innerHTML = ''
+        videoWrapper.appendChild(videoCanvas)
+        videoCanvas.srcObject = localMediaStream
+        mediaTracks = localMediaStream
+        window.mediaTracks = mediaTracks
+      },
+      function (error) {
+        if (typeof onError === 'function') onError(error)
+      }
+    )
+  }
+
+  const capture = (captureArguments = {}) => {
+    const captureOptions = {
+      type: 'image/jpeg',
+      quality: 0.85,
+      stopStreamAfterCapture: true,
+      ...captureArguments
+    }
+    const canvas = document.createElement('canvas')
+    const { width, height } = [...mediaTracks.getVideoTracks()]
+      .filter(video => video.enabled)[0]
+      .getSettings()
+    canvas.width = width
+    canvas.height = height
+    canvas.getContext('2d').drawImage(videoCanvas, 0, 0)
+
+    if (typeof camOptions.onCapture === 'function') {
+      const { type, quality } = captureOptions
+      camOptions.onCapture({
+        image: canvas.toDataURL(type, quality),
+        cropInfo: {}
+      })
+    }
+  }
+
+  const close = activateCallback => {
+    captureBtn.disabled = true
+    isPlaying = false
+    videoCanvas.srcObject = null;
+
+    [...mediaTracks.getTracks()].map(track => track.stop())
+
+    if (typeof camOptions.onClose === 'function' && activateCallback) {
+      camOptions.onClose()
+    }
+  }
+
+  const checkPermissions = async function () {
+    const permissions = await navigator.permissions.query({ name: 'camera' })
+    const result = await permissions
+
+    return result.state
+  }
+
+  const isOpen = () => isPlaying
+
+  videoElement.className = 'cameraclick'
+  wrapper.className = `${element.className || 'element'}__wrapper`
+  videoWrapper.className = `${element.className || 'element'}__video-wrapper`
+  captureBtn.className = `${element.className || 'element'}__capture`
+  captureBtn.innerHTML = options.screenshotCaption
+  captureBtn.disabled = true
+  captureBtn.onclick = capture
+  videoCanvas.autoplay = true
+  videoCanvas.width = options.width
+  videoCanvas.height = options.height
+  videoCanvas.style.objectFit = 'cover'
+
+  wrapper.appendChild(videoWrapper)
+  wrapper.appendChild(captureBtn)
+  videoElement.appendChild(wrapper)
+
+  return {
+    isOpen,
+    capture,
+    open,
+    close
+  }
+}
