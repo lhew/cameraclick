63cfc4dcf8745f79617ea2691310e7254235d3f7
Leonardo Almeida Marques <leonardo.almeidamarques@finqle.nl>
2020-02-24 17:17:12 +0100

    added different callback scenarios when opening the camera stream

--- src/cameraclick.js
+++ src/cameraclick.js
@@ -1,6 +1,5 @@
-console.log('xablau')
-export default function CameraClick (element, camOptions) {
-  let isPlaying = false
+export default function CameraClick(element, camOptions) {
+  let isPlaying = false;
 
   const options = {
     width: 320,
@@ -9,38 +8,42 @@ export default function CameraClick (element, camOptions) {
     resolution: 'auto',
     captureCaption: { start: 'Start', stop: 'Stop' },
     ...camOptions
-  }
+  };
 
-  let mediaTracks = {}
-  const videoElement = element
+  let mediaTracks = {};
+  const videoElement = element;
 
-  const captureBtn = document.createElement('button')
-  const videoCanvas = document.createElement('video')
-  const videoWrapper = document.createElement('div')
-  const wrapper = document.createElement('div')
+  const captureBtn = document.createElement('button');
+  const videoCanvas = document.createElement('video');
+  const videoWrapper = document.createElement('div');
+  const wrapper = document.createElement('div');
 
-  const open = async (onOpen, onError) => {
+  const open = async (onOpen, onSuccess, onError) => {
     if (typeof onOpen === 'function') {
-      onOpen((await checkPermissions()) === 'granted')
+      onOpen((await checkPermissions()) === 'granted');
     }
 
-    isPlaying = true
-    captureBtn.disabled = false
+    isPlaying = true;
+    captureBtn.disabled = false;
 
     navigator.getUserMedia(
       { video: true },
-      function (localMediaStream) {
-        videoWrapper.innerHTML = ''
-        videoWrapper.appendChild(videoCanvas)
-        videoCanvas.srcObject = localMediaStream
-        mediaTracks = localMediaStream
-        window.mediaTracks = mediaTracks
+      function(localMediaStream) {
+        videoWrapper.innerHTML = '';
+        videoWrapper.appendChild(videoCanvas);
+        videoCanvas.srcObject = localMediaStream;
+        mediaTracks = localMediaStream;
+        window.mediaTracks = mediaTracks;
+
+        if (typeof onSuccess === 'function') {
+          onSuccess(localMediaStream);
+        }
       },
-      function (error) {
-        if (typeof onError === 'function') onError(error)
+      function(error) {
+        if (typeof onError === 'function') onError(error);
       }
-    )
-  }
+    );
+  };
 
   const capture = (captureArguments = {}) => {
     const captureOptions = {
@@ -48,65 +51,65 @@ export default function CameraClick (element, camOptions) {
       quality: 0.85,
       stopStreamAfterCapture: true,
       ...captureArguments
-    }
-    const canvas = document.createElement('canvas')
+    };
+    const canvas = document.createElement('canvas');
     const { width, height } = [...mediaTracks.getVideoTracks()]
       .filter(video => video.enabled)[0]
-      .getSettings()
-    canvas.width = width
-    canvas.height = height
-    canvas.getContext('2d').drawImage(videoCanvas, 0, 0)
+      .getSettings();
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(videoCanvas, 0, 0);
 
     if (typeof camOptions.onCapture === 'function') {
-      const { type, quality } = captureOptions
+      const { type, quality } = captureOptions;
       camOptions.onCapture({
         image: canvas.toDataURL(type, quality),
         cropInfo: { width, height }
-      })
+      });
     }
-  }
+  };
 
   const close = activateCallback => {
-    captureBtn.disabled = true
-    isPlaying = false
+    captureBtn.disabled = true;
+    isPlaying = false;
     videoCanvas.srcObject = null;
 
-    [...mediaTracks.getTracks()].map(track => track.stop())
+    [...mediaTracks.getTracks()].map(track => track.stop());
 
     if (typeof camOptions.onClose === 'function' && activateCallback) {
-      camOptions.onClose()
+      camOptions.onClose();
     }
-  }
+  };
 
-  const checkPermissions = async function () {
-    const permissions = await navigator.permissions.query({ name: 'camera' })
-    const result = await permissions
+  const checkPermissions = async function() {
+    const permissions = await navigator.permissions.query({ name: 'camera' });
+    const result = await permissions;
 
-    return result.state
-  }
+    return result.state;
+  };
 
-  const isOpen = () => isPlaying
+  const isOpen = () => isPlaying;
 
-  videoElement.className = 'cameraclick'
-  wrapper.className = `${element.className || 'element'}__wrapper`
-  videoWrapper.className = `${element.className || 'element'}__video-wrapper`
-  captureBtn.className = `${element.className || 'element'}__capture`
-  captureBtn.innerHTML = options.screenshotCaption
-  captureBtn.disabled = true
-  captureBtn.onclick = capture
-  videoCanvas.autoplay = true
-  videoCanvas.width = options.width
-  videoCanvas.height = options.height
-  videoCanvas.style.objectFit = 'cover'
+  videoElement.className = 'cameraclick';
+  wrapper.className = `${element.className || 'element'}__wrapper`;
+  videoWrapper.className = `${element.className || 'element'}__video-wrapper`;
+  captureBtn.className = `${element.className || 'element'}__capture`;
+  captureBtn.innerHTML = options.screenshotCaption;
+  captureBtn.disabled = true;
+  captureBtn.onclick = capture;
+  videoCanvas.autoplay = true;
+  videoCanvas.width = options.width;
+  videoCanvas.height = options.height;
+  videoCanvas.style.objectFit = 'cover';
 
-  wrapper.appendChild(videoWrapper)
-  wrapper.appendChild(captureBtn)
-  videoElement.appendChild(wrapper)
+  wrapper.appendChild(videoWrapper);
+  wrapper.appendChild(captureBtn);
+  videoElement.appendChild(wrapper);
 
   return {
     isOpen,
     capture,
     open,
     close
-  }
+  };
 }
